28f4f1a91e14d3cece24895a5037dc21a351939a
aQa.Ali <khorramiali80@gmail.com>
2016-06-30 14:35:11 +0430

    Update writer2.lua

--- plugins/writer2.lua
+++ plugins/writer2.lua
@@ -186,7 +186,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."Alomina Bot"
+	return result_text.."🔥SPIRAN Bot🔥"
 end
 
 return {
