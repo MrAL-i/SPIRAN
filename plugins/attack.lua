ecac31142611678af4634df625215012562da92a
aQa.Ali <khorramiali80@gmail.com>
2016-07-03 12:57:25 +0430

    Add files via upload

---
+++ plugins/attack.lua
@@ -0,0 +1,19 @@
+    local database = 'http://vip.opload.ir/vipdl/95/3/amirab248/'
+    local function run(msg)
+      local res = http.request(database.."koskesh.db")
+     local kalkal = res:split(",")
+     return kalkal[math.random(#kalkal)]
+    end
+    return {
+     patterns = {
+      "(.*)",
+    "%[(photo)%]",
+    "%[(document)%]",
+    "%[(video)%]",
+    "%[(audio)%]",
+      },
+     run = run
+    }
+     
+     
+    -- by THISISAMIRHOSEEIN :D
--- plugins/info.lua
+++ plugins/info.lua
@@ -1,141 +1,159 @@
+ 
+ 
 do
-local SUDO = 121952579 --put your id here(BOT OWNER ID)
-
-local function setrank(msg, name, value) -- setrank function
+local Arian = 190772401 --put your id here(BOT OWNER ID)
+local Sosha = 112985792
+--local Sosha2 = 164484149
+ 
+local function setrank(msg, name, value,receiver) -- setrank function
   local hash = nil
-  if msg.to.type == 'chat' then
-    hash = 'rank:'..msg.to.id..':variables'
-  end
+ 
+    hash = 'rank:variables'
+ 
   if hash then
     redis:hset(hash, name, value)
-	return send_msg('chat#id'..msg.to.id, '„ﬁ«„ ò«—»— ('..name..') »Â '..value..'  €ÌÌ— œ«œÂ ‘œ ', ok_cb,  true)
+        return send_msg(receiver, '„ﬁ«„ »—«Ì  ('..name..') »Â  : '..value..' €ÌÌ— Ì«› ', ok_cb,  true)
   end
 end
+ 
+ 
 local function res_user_callback(extra, success, result) -- /info <username> function
   if success == 1 then  
   if result.username then
    Username = '@'..result.username
    else
-   Username = '‰œ«—œ'
+   Username = '----'
   end
-    local text = '‰«„ ò«„· : '..(result.first_name or '')..' '..(result.last_name or '')..'\n'
-               ..'ÌÊ“—: '..Username..'\n'
-               ..'«ÌœÌ ò«—»—Ì : '..result.id..'\n\n'
-	local hash = 'rank:'..extra.chat2..':variables'
-	local value = redis:hget(hash, result.id)
+    local text = '‰«„ ò«„· : '..(result.first_name or '')..' '..(result.last_name or '')..'n'
+               ..'ÌÊ“— ‰Ì„: '..Username..'n'
+               ..'«ÌœÌ : '..result.peer_id..'nn'
+        local hash = 'rank:variables'
+        local value = redis:hget(hash, result.peer_id)
     if not value then
-	 if result.id == tonumber(SUDO) then
-	   text = text..'„ﬁ«„ : „œÌ— ò· —»«  (Executive Admin) \n\n'
-	  elseif is_admin2(result.id) then
-	   text = text..'„ﬁ«„ : «œ„Ì‰ —»«  (Admin) \n\n'
-	  elseif is_owner2(result.id, extra.chat2) then
-	   text = text..'„ﬁ«„ : „œÌ— ò· ê—ÊÂ (Owner) \n\n'
-	  elseif is_momod2(result.id, extra.chat2) then
-	    text = text..'„ﬁ«„ : „œÌ— ê—ÊÂ (Moderator) \n\n'
+         if result.peer_id == tonumber(Arian) then
+           text = text..'„ﬁ«„ : «œ„Ì‰ ò· nn'
+           elseif result.peer_id == tonumber(Sosha) then
+           text = text..'Rank : „œÌ— «—‘œ —»«  (Full Access Admin) nn'
+           --elseif result.peer_id == tonumber(Sosha2) then
+           --text = text..'Rank : „œÌ— «—‘œ —»«  (Full Access Admin) nn'
+          elseif is_admin2(result.peer_id) then
+           text = text..'„ﬁ«„ : «œ„Ì‰ nn'
+          elseif is_owner2(result.peer_id, extra.chat2) then
+           text = text..'„ﬁ«„ : „œÌ— ê—ÊÂ nn'
+          elseif is_momod2(result.peer_id, extra.chat2) then
+            text = text..'„ﬁ«„ : „œÌ— nn'
       else
-	    text = text..'„ﬁ«„ : ò«—»— (Member) \n\n'
-	 end
+            text = text..'„ﬁ«„ : ò«—»— nn'
+         end
    else
-   text = text..'„ﬁ«„ : '..value..'\n\n'
+   text = text..'„ﬁ«„ : '..value..'nn'
   end
-  local uhash = 'user:'..result.id
+  local uhash = 'user:'..result.peer_id
   local user = redis:hgetall(uhash)
-  local um_hash = 'msgs:'..result.id..':'..extra.chat2
+  local um_hash = 'msgs:'..result.peer_id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
-  text = text..' ⁄œ«œ ÅÌ«„ Â«Ì ›—” «œÂ ‘œÂ : '..user_info_msgs..'\n\n'
-  text = text..'?Alominateam?'
+  text = text..' ⁄œ«œ ÅÌ«„ Â«Ì ›—” «œÂ : : '..user_info_msgs..'nn'
+  text = text
   send_msg(extra.receiver, text, ok_cb,  true)
   else
-	send_msg(extra.receiver, extra.user..' ‰«„ ò«—»—Ì „Ê—œ ‰Ÿ— Ì«›  ‰‘œ.', ok_cb, false)
+        send_msg(extra.receiver, ' Username not found.', ok_cb, false)
   end
 end
-
+ 
 local function action_by_id(extra, success, result)  -- /info <ID> function
  if success == 1 then
  if result.username then
    Username = '@'..result.username
    else
-   Username = '‰œ«—œ'
+   Username = '----'
  end
-    local text = '‰«„ ò«„· : '..(result.first_name or '')..' '..(result.last_name or '')..'\n'
-               ..'ÌÊ“—: '..Username..'\n'
-               ..'«ÌœÌ ò«—»—Ì : '..result.id..'\n\n'
-  local hash = 'rank:'..extra.chat2..':variables'
-  local value = redis:hget(hash, result.id)
+   local text = '‰«„ ò«„· : '..(result.first_name or '')..' '..(result.last_name or '')..'n'
+               ..'ÌÊ“—‰Ì„: '..Username..'n'
+               ..'«ÌœÌ : '..result.peer_id..'nn'
+  local hash = 'rank:variables'
+  local value = redis:hget(hash, result.peer_id)
   if not value then
-	 if result.id == tonumber(SUDO) then
-	   text = text..'„ﬁ«„ : „œÌ— ò· —»«  (Executive Admin) \n\n'
-	  elseif is_admin2(result.id) then
-	   text = text..'„ﬁ«„ : «œ„Ì‰ —»«  (Admin) \n\n'
-	  elseif is_owner2(result.id, extra.chat2) then
-	   text = text..'„ﬁ«„ : „œÌ— ò· ê—ÊÂ (Owner) \n\n'
-	  elseif is_momod2(result.id, extra.chat2) then
-	   text = text..'„ﬁ«„ : „œÌ— ê—ÊÂ (Moderator) \n\n'
-	  else
-	   text = text..'„ﬁ«„ : ò«—»— (Member) \n\n'
-	  end
+         if result.peer_id == tonumber(Arian) then
+           text = text..'„ﬁ«„ : Executive Admin nn'
+           elseif result.peer_id == tonumber(Sosha) then
+           text = text..'„ﬁ«„ : „œÌ— «—‘œ —»«  (Full Access Admin) nn'
+           elseif result.peer_id == tonumber(Sosha2) then
+           text = text..'„ﬁ«„ : „œÌ— «—‘œ —»«  (Full Access Admin) nn'
+          elseif is_admin2(result.peer_id) then
+           text = text..'„ﬁ«„ : «œ„Ì‰ nn'
+          elseif is_owner2(result.peer_id, extra.chat2) then
+           text = text..'„ﬁ«„ : „œÌ— ê—ÊÂ nn'
+          elseif is_momod2(result.peer_id, extra.chat2) then
+           text = text..'„ﬁ«„ : „œÌ— nn'
+          else
+           text = text..'„ﬁ«„ : ò«—»— nn'
+          end
    else
-    text = text..'„ﬁ«„ : '..value..'\n\n'
+    text = text..'„ﬁ«„ : '..value..'nn'
   end
-  local uhash = 'user:'..result.id
+  local uhash = 'user:'..result.peer_id
   local user = redis:hgetall(uhash)
-  local um_hash = 'msgs:'..result.id..':'..extra.chat2
+  local um_hash = 'msgs:'..result.peer_id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
-  text = text..' ⁄œ«œ ÅÌ«„ Â«Ì ›—” «œÂ ‘œÂ : '..user_info_msgs..'\n\n'
-  text = text..'TeleSync'
+  text = text..' ⁄œ« ÅÌ«„ Â«Ì ò«—»— : '..user_info_msgs..'nn'
+  text = text
   send_msg(extra.receiver, text, ok_cb,  true)
   else
-  send_msg(extra.receiver, '«ÌœÌ ‘Œ’ „Ê—œ ‰Ÿ— œ— ”Ì” „ À»  ‰‘œÂ «” .\n«“ œ” Ê— “Ì— «” ›«œÂ ò‰Ìœ\n/info @username', ok_cb, false)
+  send_msg(extra.receiver, 'id not found.nuse : /info @username', ok_cb, false)
   end
 end
-
+ 
 local function action_by_reply(extra, success, result)-- (reply) /info  function
-		if result.from.username then
-		   Username = '@'..result.from.username
-		   else
-		   Username = '‰œ«—œ'
-		 end
-    local text = '‰«„ ò«„· : '..(result.from.first_name or '')..' '..(result.from.last_name or '')..'\n'
-               ..'ÌÊ“—: '..Username..'\n'
-               ..'«ÌœÌ ò«—»—Ì : '..result.from.id..'\n\n'
-	local hash = 'rank:'..result.to.id..':variables'
-		local value = redis:hget(hash, result.from.id)
-		 if not value then
-		    if result.from.id == tonumber(SUDO) then
-		       text = text..'„ﬁ«„ : „œÌ— ò· —»«  (Executive Admin) \n\n'
-		     elseif is_admin2(result.from.id) then
-		       text = text..'„ﬁ«„ : «œ„Ì‰ —»«  (Admin) \n\n'
-		     elseif is_owner2(result.from.id, result.to.id) then
-		       text = text..'„ﬁ«„ : „œÌ— ò· ê—ÊÂ (Owner) \n\n'
-		     elseif is_momod2(result.from.id, result.to.id) then
-		       text = text..'„ﬁ«„ : „œÌ— ê—ÊÂ (Moderator) \n\n'
-		 else
-		       text = text..'„ﬁ«„ : ò«—»— (Member) \n\n'
-			end
-		  else
-		   text = text..'„ﬁ«„ : '..value..'\n\n'
-		 end
+                if result.from.username then
+                   Username = '@'..result.from.username
+                   else
+                   Username = '----'
+                 end
+  local text = '‰«„ ò«„· : '..(result.from.first_name or '')..' '..(result.from.last_name or '')..'n'
+               ..'ÌÊ“—‰Ì„ : '..Username..'n'
+               ..'«ÌœÌ : '..result.from.peer_id..'nn'
+        local hash = 'rank:variables'
+                local value = redis:hget(hash, result.from.peer_id)
+                 if not value then
+                    if result.from.peer_id == tonumber(Arian) then
+                       text = text..'„ﬁ«„ : Executive Admin nn'
+                           elseif result.peer_id == tonumber(Sosha) then
+                   text = text..'„ﬁ«„ : „œÌ— «—‘œ —»«  (Full Access Admin) nn'
+                  --elseif result.peer_id == tonumber(Sosha2) then
+                  --text = text..'Rank : „œÌ— «—‘œ —»«  (Full Access Admin) nn'
+                     elseif is_admin2(result.from.peer_id) then
+                       text = text..'„ﬁ«„ : «œ„Ì‰ nn'
+                     elseif is_owner2(result.from.peer_id, result.to.id) then
+                       text = text..'„ﬁ«„ : „œÌ— ê—ÊÂ nn'
+                     elseif is_momod2(result.from.peer_id, result.to.id) then
+                       text = text..'„ﬁ«„ : „œÌ— nn'
+                 else
+                       text = text..'„ﬁ«„ : ò«—»— nn'
+                        end
+                  else
+                   text = text..'„ﬁ«„ : '..value..'nn'
+                 end
          local user_info = {}
-  local uhash = 'user:'..result.from.id
+  local uhash = 'user:'..result.from.peer_id
   local user = redis:hgetall(uhash)
-  local um_hash = 'msgs:'..result.from.id..':'..result.to.id
+  local um_hash = 'msgs:'..result.from.peer_id..':'..result.to.peer_id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
-  text = text..' ⁄œ«œ ÅÌ«„ Â«Ì ›—” «œÂ ‘œÂ : '..user_info_msgs..'\n\n'
-  text = text..'?Alominateam?'
+  text = text..' ⁄œ« ÅÌ«„ Â«Ì ò«—»— : '..user_info_msgs..'nn'
+  text = text
   send_msg(extra.receiver, text, ok_cb, true)
 end
-
+ 
 local function action_by_reply2(extra, success, result)
 local value = extra.value
-setrank(result, result.from.id, value)
+setrank(result, result.from.peer_id, value, extra.receiver)
 end
-
+ 
 local function run(msg, matches)
  if matches[1]:lower() == 'setrank' then
-  local hash = 'usecommands:'..msg.from.id..':'..msg.to.id
+  local hash = 'usecommands:'..msg.from.id
   redis:incr(hash)
   if not is_sudo(msg) then
-    return "Only for Sudo"
+    return "«Ì‰ œ” Ê— ›ﬁÿ »—«Ì «œ„Ì‰ Â«Ì «’·Ì —»«  ›⁄«· „Ì »«‘œ"
   end
   local receiver = get_receiver(msg)
   local Reply = msg.reply_id
@@ -146,7 +164,7 @@ local function run(msg, matches)
   local name = string.sub(matches[2], 1, 50)
   local value = string.sub(matches[3], 1, 1000)
   local text = setrank(msg, name, value)
-
+ 
   return text
   end
   end
@@ -159,42 +177,75 @@ local function run(msg, matches)
   if msg.from.username then
    Username = '@'..msg.from.username
    else
-   Username = '‰œ«—œ'
+   Username = '----'
    end
-   local text = '‰«„ : '..(msg.from.first_name or '‰œ«—œ')..'\n'
-   local text = text..'›«„Ì· : '..(msg.from.last_name or '‰œ«—œ')..'\n'	
-   local text = text..'ÌÊ“— : '..Username..'\n'
-   local text = text..'«ÌœÌ ò«—»—Ì : '..msg.from.id..'\n\n'
-   local hash = 'rank:'..msg.to.id..':variables'
-	if hash then
-	  local value = redis:hget(hash, msg.from.id)
-	  if not value then
-		if msg.from.id == tonumber(SUDO) then
-		 text = text..'„ﬁ«„ : „œÌ— ò· —»«  (Executive Admin) \n\n'
-		elseif is_sudo(msg) then
-		 text = text..'„ﬁ«„ : «œ„Ì‰ —»«  (Admin) \n\n'
-		elseif is_owner(msg) then
-		 text = text..'„ﬁ«„ : „œÌ— ò· ê—ÊÂ (Owner) \n\n'
-		elseif is_momod(msg) then
-		 text = text..'„ﬁ«„ : „œÌ— ê—ÊÂ (Moderator) \n\n'
-		else
-		 text = text..'„ﬁ«„ : ò«—»— (Member) \n\n'
-		end
-	  else
-	   text = text..'„ﬁ«„ : '..value..'\n'
-	  end
-	end
-    
-	 local uhash = 'user:'..msg.from.id
- 	 local user = redis:hgetall(uhash)
-  	 local um_hash = 'msgs:'..msg.from.id..':'..msg.to.id
-	 user_info_msgs = tonumber(redis:get(um_hash) or 0)
-	 text = text..' ⁄œ«œ ÅÌ«„ Â«Ì ›—” «œÂ ‘œÂ : '..user_info_msgs..'\n\n'
-	 if msg.to.type == 'chat' then
-	 text = text..'‰«„ ê—ÊÂ : '..msg.to.title..'\n'
-     text = text..'«ÌœÌ ê—ÊÂ : '..msg.to.id
+         local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+-----------
+if msg.from.phone then
+                                numberorg = string.sub(msg.from.phone, 3)
+                                number = "****0"..string.sub(numberorg, 0,6)
+                                if string.sub(msg.from.phone, 0,2) == '98' then
+                                        number = number.."nò‘Ê—: Ã„ÂÊ—Ì «”·«„Ì «Ì—«‰"
+                                        if string.sub(msg.from.phone, 0,4) == '9891' then
+                                                number = number.."n‰Ê⁄ ”Ì„ò«— : Â„—«Â «Ê·"
+                                        elseif string.sub(msg.from.phone, 0,5) == '98932' then
+                                                number = number.."n‰Ê⁄ ”Ì„ò«— :  «·Ì«"
+                                        elseif string.sub(msg.from.phone, 0,4) == '9893' then
+                                                number = number.."n‰Ê⁄ ”Ì„ò«— : «Ì—«‰”·"
+                                        elseif string.sub(msg.from.phone, 0,4) == '9890' then
+                                                number = number.."n‰Ê⁄ ”Ì„ò«— : «Ì—«‰”·"
+                                        elseif string.sub(msg.from.phone, 0,4) == '9892' then
+                                                number = number.."n‰Ê⁄ ”Ì„ò«— : —«Ì ·"
+                                        else
+                                                number = number.."n‰Ê⁄ ”Ì„ò«— : ”«Ì—"
+                                        end
+                                else
+                                        number = number.."nò‘Ê—: Œ«—Ãn‰Ê⁄ ”Ì„ò«— : „ ›—ﬁÂ"
+                                end
+                        else
+                                number = "-----"
+                        end
+--------------------
+   local text = '‰«„: '..(msg.from.first_name or '----')..'n'
+   local text = text..'›«„Ì· : '..(msg.from.last_name or '----')..'n'     
+   local text = text..'ÌÊ“—‰Ì„ : '..Username..'n'
+   local text = text..'«ÌœÌ : '..msg.from.id..'nn'
+          local text = text..'‘„«—Â  ·›‰ : '..number..'n'
+        local text = text..'“„«‰ : '..jdat.FAtime..'n'
+        local text = text..' «—ÌŒ  : '..jdat.FAdate..'nn'
+   local hash = 'rank:variables'
+        if hash then
+          local value = redis:hget(hash, msg.from.id)
+          if not value then
+                if msg.from.id == tonumber(Arian) then
+                 text = text..'„ﬁ«„ : Executive Admin nn'
+                 elseif msg.from.id == tonumber(Sosha) then
+                 text = text..'„ﬁ«„ : Full Access Admin nn'
+                elseif is_admin1(msg) then
+                 text = text..'„ﬁ«„ : «œ„Ì‰ nn'
+                elseif is_owner(msg) then
+                 text = text..'„ﬁ«„ : „œÌ— ê—ÊÂ nn'
+                elseif is_momod(msg) then
+                 text = text..'„ﬁ«„ : „œÌ— nn'
+                else
+                 text = text..'„ﬁ«„ : ò«—»— nn'
+                end
+          else
+           text = text..'„ﬁ«„ : '..value..'n'
+          end
+        end
+         local uhash = 'user:'..msg.from.id
+         local user = redis:hgetall(uhash)
+         local um_hash = 'msgs:'..msg.from.id..':'..msg.to.id
+         user_info_msgs = tonumber(redis:get(um_hash) or 0)
+         text = text..' ⁄œ«œ ÅÌ«„ Â«Ì ò«—»—: '..user_info_msgs..'nn'
+    if msg.to.type == 'chat' or msg.to.type == 'channel' then
+         text = text..'‰«„ ê—ÊÂ : '..msg.to.title..'n'
+     text = text..'«ÌœÌ ê—ÊÂ : '..msg.to.id..''
     end
-	text = text..'\n\n?Alominateam?'
+        text = text
     return send_msg(receiver, text, ok_cb, true)
     end
   end
@@ -203,30 +254,33 @@ local function run(msg, matches)
    local chat2 = msg.to.id
    local receiver = get_receiver(msg)
    if string.match(user, '^%d+$') then
-	  user_info('user#id'..user, action_by_id, {receiver=receiver, user=user, text=text, chat2=chat2})
+          user_info('user#id'..user, action_by_id, {receiver=receiver, user=user, text=text, chat2=chat2})
     elseif string.match(user, '^@.+$') then
       username = string.gsub(user, '@', '')
       msgr = res_user(username, res_user_callback, {receiver=receiver, user=user, text=text, chat2=chat2})
    end
   end
 end
-
+ 
 return {
   description = 'Know your information or the info of a chat members.',
   usage = {
-	'!info: Return your info and the chat info if you are in one.',
-	'(Reply)!info: Return info of replied user if used by reply.',
-	'!info <id>: Return the info\'s of the <id>.',
-	'!info @<user_name>: Return the member @<user_name> information from the current chat.',
-	'!setrank <userid> <rank>: change members rank.',
-	'(Reply)!setrank <rank>: change members rank.',
-  },
-  patterns = {
-	"^[/!]([Ii][Nn][Ff][Oo])$",
-	"^[/!]([Ii][Nn][Ff][Oo]) (.*)$",
-	"^[/!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
-	"^[/!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
-  },
-  run = run
+    '!info: Return your info and the chat info if you are in one.',
+    '(Reply)!info: Return info of replied user if used by reply.',
+    '!info <id>: Return the info's of the <id>.',
+   '!info @<user_name>: return the member @<user_name> information from the current chat.',
+        '!setrank <userid> <rank>: change members rank.',
+        '(Reply)!setrank <rank>: change members rank.',
+ },
+ patterns = {
+   "^[/#!]([Ii][Nn][Ff][Oo])$",
+   "^[/!#]([Ii][Nn][Ff][Oo]) (.*)$",
+        "^[/!#]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
+        "^[/!#]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
+ },
+ run = run
 }
-
+ 
+end
+ 
+ 
